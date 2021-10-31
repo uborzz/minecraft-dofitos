c6130e8cd7cb8664f0fa4389decc12a5c1f26ffd
uborzz <uborzz@gmail.com>
2021-10-31 15:21:41 +0100

    stop before backup

--- scripts/add_to_cron.sh
+++ scripts/add_to_cron.sh
@@ -4,8 +4,7 @@ base=$(dirname "$0")"/../"
 real=$(realpath $base)
 
 sudo crontab -l > /tmp/cron &> /dev/null
-echo "3 0 * * * $real/scripts/backup.sh" >> /tmp/cron
-echo "15 0 * * * $real/scripts/backup.sh" >> /tmp/cron
+echo "0 5 * * * $real/scripts/backup.sh" >> /tmp/cron
 sudo crontab /tmp/cron
 
 echo "Your sudo crontab:"
--- scripts/backup.sh
+++ scripts/backup.sh
@@ -3,4 +3,7 @@
 base=$(dirname "$0")"/../"
 now=$(date +"%y%m%d-%H%M%S")
 mkdir -p $base/backups/$now
+
+make stop -C $base
 cp -rp $base/minecraft-data $base/backups/$now
+make run -C $base
